f93218225ae303cce6c963f08734074163b0a348
viccsen <252704620@qq.com>
2020-06-29 16:50:03 +0800

    upgrade: umi2 => um3

--- .umirc.ts
+++ .umirc.ts
@@ -1,30 +1,11 @@
-import { IConfig } from 'umi-types';
+import { defineConfig } from 'umi';
 
-// ref: https://umijs.org/config/
-const config: IConfig =  {
-  treeShaking: true,
-  plugins: [
-    // ref: https://umijs.org/plugin/umi-plugin-react.html
-    ['umi-plugin-react', {
-      antd: true,
-      dva: true,
-      dynamicImport: { webpackChunkName: true },
-      title: 'bugu-management',
-      dll: true,
-
-      routes: {
-        exclude: [
-          /models\//,
-          /utils\//,
-          /caches\//,
-          /services\//,
-          /model\.(t|j)sx?$/,
-          /service\.(t|j)sx?$/,
-          /components\//,
-        ],
-      },
-    }],
-  ],
+export default defineConfig({
+  title: 'bugu-management',
+  antd: {},
+  dva: {
+    skipModelValidate: true
+  },
   hash: true,
   // proxy: {
   //   '/api/hii/*': {
@@ -35,6 +16,5 @@ const config: IConfig =  {
   theme: {
     '@primary-color': '#1DA57A',
   },
-}
+})
 
-export default config;
