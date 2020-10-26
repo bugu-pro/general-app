b74b8656ad64076ff05299d8b8e9ed413b4157f9
viccsen <252704620@qq.com>
2020-10-26 11:56:04 +0800

    add: 新增web模板

--- .umirc.ts
+++ .umirc.ts
@@ -9,28 +9,10 @@ export default defineConfig({
   dynamicImport: {
     loading: '@/Loading',
   },
-  plugins: ['@alitajs/hd'],
-  hd: {
-    // same as config.theme, default set @hd: 2px
-    theme: {},
-    px2rem: {
-      rootValue: 100,
-      unitPrecision: 5,
-      propWhiteList: [],
-      propBlackList: [],
-      exclude: false,
-      selectorBlackList: [],
-      ignoreIdentifier: false,
-      replace: true,
-      mediaQuery: false,
-      minPixelValue: 0,
-    },
+  targets: {
+    ie: 11,
   },
   hash: true,
-  targets: {
-    ios: '8.4',
-    android: '4.2',
-  },
   // proxy: {
   //   '/api/hii/*': {
   //     'target': 'http://smart-campus-mp-test.yunzhiyuan100.com.cn/',
@@ -40,4 +22,28 @@ export default defineConfig({
   theme: {
     '@primary-color': '#1DA57A',
   },
+  // 页面过多，文件资源较大但情况下开启动态加载
+  // chunks: ['vendors', 'umi'],
+  // chainWebpack: function (config, { webpack }) {
+  //   config.merge({
+  //     optimization: {
+  //       minimize: true,
+  //       splitChunks: {
+  //         chunks: 'all',
+  //         minSize: 30000,
+  //         minChunks: 3,
+  //         automaticNameDelimiter: '.',
+  //         cacheGroups: {
+  //           vendor: {
+  //             name: 'vendors',
+  //             test({ resource }) {
+  //               return /[\\/]node_modules[\\/]/.test(resource);
+  //             },
+  //             priority: 10,
+  //           },
+  //         },
+  //       },
+  //     }
+  //   });
+  // },
 });
--- src/components/Page/index.tsx
+++ src/components/Page/index.tsx
@@ -1,9 +1,8 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Spin } from 'antd';
 import classnames from 'classnames';
 import styles from './index.less';
-import Header from './header';
-import {Icon} from 'antd-mobile';
 
 export interface PageProps {
   id?: string;
@@ -12,18 +11,12 @@ export interface PageProps {
   children?: string | React.ReactNode;
   className?: string;
   style?: React.CSSProperties;
-  footerIsFixed?: boolean;
-  mainIsFixed?: boolean;
-  headerIsFixed?: boolean;
   htmlTitle?: string;
   loading?: boolean;
 }
 
 export default class Page extends Component<PageProps, any> {
-
   static CID = 1;
-
-  static Header = Header;
 
   static propTypes = {
     id: PropTypes.string,
@@ -31,9 +24,6 @@ export default class Page extends Component<PageProps, any> {
     footer: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
     className: PropTypes.string,
     style: PropTypes.object,
-    footerIsFixed: PropTypes.bool,
-    mainIsFixed: PropTypes.bool,
-    headerIsFixed: PropTypes.bool,
     htmlTitle: PropTypes.string,
     loading: PropTypes.bool,
   };
@@ -47,18 +37,7 @@ export default class Page extends Component<PageProps, any> {
   id = 'Page-' + Page.CID++;
 
   render() {
-    const {
-      id,
-      title,
-      children,
-      footer,
-      className,
-      style,
-      footerIsFixed,
-      mainIsFixed,
-      headerIsFixed,
-      loading,
-    } = this.props;
+    const { id, title, children, footer, className, style, loading } = this.props;
 
     const htmlTitle = this.props.htmlTitle || (typeof title === 'string' ? title : null);
     if (htmlTitle && htmlTitle !== document.title) {
@@ -68,24 +47,16 @@ export default class Page extends Component<PageProps, any> {
     const props = {
       role: 'Page',
       id: id || this.id,
-      className: classnames(styles['page'], className, {
-        [styles['page-footer-fixed']]: footerIsFixed,
-        [styles['page-main-fixed']]: mainIsFixed,
-        [styles['page-header-fixed']]: headerIsFixed,
-        [styles['page-loading']]: loading,
-      }),
+      className: classnames(styles['page'], className),
       style,
     };
     return (
       <section {...props}>
-        <header>{typeof title === 'string' ? <h1>{title}</h1> : title}</header>
-        <main>{children}</main>
-        <footer>{footer}</footer>
-        {loading && (
-          <div className={styles['loading']}>
-            <Icon type="loading" size="lg"/>
-          </div>
-        )}
+        <Spin spinning={!!loading}>
+          <header>{typeof title === 'string' ? <h1>{title}</h1> : title}</header>
+          <main>{children}</main>
+          <footer>{footer}</footer>
+        </Spin>
       </section>
     );
   }
