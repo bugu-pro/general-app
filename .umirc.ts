f04202b544da96f86be1f20572de416ef446f14d
viccsen <252704620@qq.com>
2021-08-02 19:01:00 +0800

    【feat】umi配置更新

--- .umirc.ts
+++ .umirc.ts
@@ -6,6 +6,13 @@ export default defineConfig({
   dva: {
     skipModelValidate: true
   },
+  mfsu: {},
+  fastRefresh: {},
+  nodeModulesTransform: {
+    type: 'none', //不编译npm库的代码，提升编译速度
+    exclude: [], //如果npm库为es6代码，可通过 exclude 配置添加额外需要编译的
+  },
+  webpack5: {},
   hash: true,
   // proxy: {
   //   '/api/hii/*': {
@@ -16,30 +23,5 @@ export default defineConfig({
   theme: {
     '@primary-color': '#1DA57A',
   },
-  // 页面过多，文件资源较大但情况下开启动态加载
-  // dynamicImport: {loading: '@/components/Loading'},
-  // chunks: ['vendors', 'umi'],
-  // chainWebpack: function (config, { webpack }) {
-  //   config.merge({
-  //     optimization: {
-  //       minimize: true,
-  //       splitChunks: {
-  //         chunks: 'all',
-  //         minSize: 30000,
-  //         minChunks: 3,
-  //         automaticNameDelimiter: '.',
-  //         cacheGroups: {
-  //           vendor: {
-  //             name: 'vendors',
-  //             test({ resource }) {
-  //               return /[\\/]node_modules[\\/]/.test(resource);
-  //             },
-  //             priority: 10,
-  //           },
-  //         },
-  //       },
-  //     }
-  //   });
-  // },
 })
 
